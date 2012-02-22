89eebf0efc3287a5ba1b26564454f303d304f81b
Andrea Fiore <and@inventati.org>
2012-02-22 15:14:53 +0000

    added make release script

---
+++ web/make-release.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+git status|grep "working directory clean" > /dev/null
+
+if [ $? === 0 ]
+  echo "Git index is clean, going ahead"
+else
+  echo "You have uncommited changes; commit first, or you might loose data!"
+  exit 1
+fi
+
+
+cake build && cake minify
+timestamp=`coffee -e 'console.info (+ new Date)'`
+tmpdir="/tmp/${timestamp}-public"
+releasename=`cake release-name`
+
+echo "git add js/*.js"
+echo "git commit -m 'built new release: ${releasename}'"
+echo "git tag -a ${releasename}"
+
+cp -r public $tmpdir
+git checkout gh-pages
+cd .. && rm .git/index && git clean -fdx
+cp $tmpdir/* .
